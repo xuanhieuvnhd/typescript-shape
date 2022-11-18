8c7761fdea5078f389eaf8d87b93748cbbdc6c02
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-18 07:54:13 +0700

    18/11

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rectangle_1 = require("./rectangle");
+const rectangle = new rectangle_1.Rectangle(5, 5, 10, 20);
+console.log("----------------------------------------------")
+console.log(rectangle.toString());
+console.log("Diện tích hình chữ nhật là: "+rectangle.area());
+console.log("----------------------------------------------")
+//# sourceMappingURL=main.js.map
